c715c9027ab29cd7fbfc1dcefd9a696bedfd8d17
Peter Alagoa <peteralagoa@gmail.com>
2022-09-20 18:15:44 -0700

    feste

--- resources/js/screens/Explore.jsx
+++ resources/js/screens/Explore.jsx
@@ -37,7 +37,48 @@ React.useEffect(()=>{
 },[])
   return (
    <MainContainer select="explore">
+<div className='flex md:flex-row flex-col w-full bg-red-400 justify-around p-2 items-center gap-4'>
+    <div className='flex-1 w-full'>
+        <p>I'm a</p>
+        <select className='ring-1 p-2 ring-slate-200 bg-transparent w-full'>
+            <option>male</option>
+            <option>female</option>
+        </select>
+    </div>
+    <div className='flex-1 w-full'>
+        <p>Seeking a</p>
+        <select className='ring-1 p-2 ring-slate-200 bg-transparent w-full'>
+            <option>male</option>
+            <option>any</option>
+            <option>male</option>
+            <option>female</option>
+        </select>
+    </div>
+    <div className='flex-1 w-full'>
+        <p>From</p>
+       <select className='ring-1 p-2 ring-slate-200 bg-transparent w-full'>
+            <option>male</option>
+            <option>nigeria</option>
+        </select>
+    </div>
+    <div className='flex-1 w-full'>
+        <p>Age</p>
+        <div className='flex md:flex-row flex-col gap-4'>
+        <select className='ring-1 p-2 ring-slate-200 bg-transparent md:w-1/2 w-full'>
+            <option>male</option>
+            <option>18</option>
+        </select>
+        <select className='ring-1 p-2 ring-slate-200 bg-transparent md:w-1/2 w-full'>
+            <option>male</option>
+            <option>50</option>
+        </select>
+        </div>
+    </div>
+    <div className='flex-1 w-full'>
 
+        <button className='bg-yellow-400 justify-center items-center p-2 w-full rounded-full font-bold hover:bg-white'>Send</button>
+    </div>
+</div>
         <div className='md:w-full mx-auto  h-screen'  >
             <div className='flex gap-3 bg-zinc-100 p-3'>
                 <img src={data.group} />
