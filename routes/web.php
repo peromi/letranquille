ea050fa049d741433777ab6a93b601ad19765050
peter <peteralagoa@gmail.com>
2022-10-20 11:26:56 -0700

    view cache

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // Artisan::call('migrate:fresh');
     Artisan::call('storage:link');
-    Artisan::call('view:clear');
-    Artisan::call('config:clear');
-    Artisan::call('route:clear');
+    // Artisan::call('view:clear');
+    // Artisan::call('config:clear');
+    // Artisan::call('route:clear');
+    Artisan::call('view:cache');
     return view('welcome');
 });
 
